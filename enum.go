09ecadbe1b38818abcb9766efe2d18f2bcbd10a6
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-02-13 08:06:19 +0000

    Posting

--- enum.go
+++ enum.go
@@ -2,3 +2,16 @@
 package hdwallet
 
 // mnemonic language
+const (
+	English            = "english"
+	ChineseSimplified  = "chinese_simplified"
+	ChineseTraditional = "chinese_traditional"
+)
+
+// zero is deafult of uint32
+const (
+	Zero      uint32 = 0
+	ZeroQuote uint32 = 0x80000000
+	BTCToken  uint32 = 0x10000000
+	ETHToken  uint32 = 0x20000000
+)
