45ecdc82f9b678c23ec60336f6ecc3cc4ba32cbe
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-02-14 13:18:26 +0000

    Update timetable.md

--- example/createRawTransaction/main.go
+++ example/createRawTransaction/main.go
@@ -20,3 +20,15 @@ var (
 )
 
 // utxo is an unspent transaction output of wallet
+type utxo struct {
+	key    *hdwallet.Key
+	hash   string // hash of this utxo. example: f75f1b0810857dff6972767d745a57d0aeb77ae4845ef4f698a7bd50ec34a4b4 (BTC Testnet)
+	index  uint32 // index of this wallet at this utxo
+	amount int64  // satoshis amount of this utxo
+}
+
+func (u *utxo) ToTxIn() (*wire.TxIn, error) {
+	hash, err := chainhash.NewHashFromStr(u.hash)
+	if err != nil {
+		return nil, err
+	}
