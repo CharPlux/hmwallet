090dd893522484ea0618362b5101682e5ac2a039
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-02-14 08:44:41 +0000

    Bumping knative-prow

--- example/createRawTransaction/main.go
+++ example/createRawTransaction/main.go
@@ -1,2 +1,15 @@
 
 package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/foxnut/go-hdwallet"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcutil"
