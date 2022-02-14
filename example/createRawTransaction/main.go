fb7356fbc98c813df5978cea9f70a59deaa0979b
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-02-14 11:43:17 +0000

    added some screenshots

--- example/createRawTransaction/main.go
+++ example/createRawTransaction/main.go
@@ -13,3 +13,10 @@ import (
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
+)
+
+var (
+	mnemonic = "range sheriff try enroll deer over ten level bring display stamp recycle"
+)
+
+// utxo is an unspent transaction output of wallet
