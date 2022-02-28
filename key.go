fa967bbf7c9a04abfaee31d336e92a44b2fb4784
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-02-28 15:10:49 +0000

    Maven artifacts for RMI Messaging version 1.1.5-SNAPSHOT

--- key.go
+++ key.go
@@ -4,3 +4,17 @@ package hdwallet
 import (
 	"crypto/ecdsa"
 	"encoding/hex"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcutil"
+	"github.com/btcsuite/btcutil/hdkeychain"
+	"github.com/cpacia/bchutil"
+)
+
+// Key struct
+type Key struct {
+	Opt      *Options
+	Extended *hdkeychain.ExtendedKey
+
+	// for btc
