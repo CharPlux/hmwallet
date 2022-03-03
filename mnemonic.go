1c19753b78eb05b17ca29379da6d787367c5a894
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-03-03 13:47:16 +0000

    Update LuCI-feed to yesterdays HEAD of master

--- mnemonic.go
+++ mnemonic.go
@@ -7,3 +7,18 @@ import (
 )
 
 func setLanguage(language string) {
+	switch language {
+	case ChineseSimplified:
+		bip39.SetWordList(wordlists.ChineseSimplified)
+	case ChineseTraditional:
+		bip39.SetWordList(wordlists.ChineseTraditional)
+	}
+}
+
+// NewMnemonic creates a random mnemonic
+func NewMnemonic(length int, language string) (string, error) {
+	setLanguage(language)
+
+	if length < 12 {
+		length = 12
+	}
