ef6fadf08d052d8b7a83397559adbb9500ce5349
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-03-03 20:56:31 +0000

    Updated installer

--- option.go
+++ option.go
@@ -11,3 +11,21 @@ import (
 // default options
 var (
 	DefaultParams = &BTCParams
+
+	// master key options
+	DefaultPassword = ""
+	DefaultLanguage = ""
+
+	// child key options
+	DefaultPurpose      = ZeroQuote + 44
+	DefaultCoinType     = BTC
+	DefaultAccount      = ZeroQuote
+	DefaultChange       = Zero
+	DefaultAddressIndex = Zero
+)
+
+// Option of key
+type Option func(*Options)
+
+// Options of key
+type Options struct {
