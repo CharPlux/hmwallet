d43adaf89b6eae9a468025fb1e1a11e73140ea29
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-04-14 07:05:52 +0000

    mmod theme

--- params.go
+++ params.go
@@ -25,4 +25,5 @@ func init() {
 	LTCParams.PrivateKeyID = 0xb0     // 176
 
 	// doge net params
-	// https://github.com/dogecoin/dogecoin/blob/master/src/chainparams
+	// https://github.com/dogecoin/dogecoin/blob/master/src/chainparams.cpp
+	DOGE
