644fb3ad83d73848cb0d47c51d323dec3b66d043
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-04-29 20:02:58 +0000

    Run scan-local.sh on influxdb:...

--- params.go
+++ params.go
@@ -33,4 +33,4 @@ func init() {
 	// dash net params
 	// https://github.com/dashpay/dash/blob/master/src/chainparams.cpp
 	DASHParams.PubKeyHashAddrID = 0x4c // 76
-	DASHParams.ScriptHashAddrID
+	DASHParams.ScriptHashAddrID = 0x10 
