921913fa2a2284735081bf7084accd2c0e2cd211
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-05-13 17:43:58 +0000

    crypto: gcm - fix assumption that assoc has one segment

--- params.go
+++ params.go
@@ -38,4 +38,4 @@ func init() {
 
 	// bch net params
 	// https://github.com/Bitcoin-ABC/bitcoin-abc/blob/master/src/chainparams.cpp
-	BCHParams.PubKeyHashAddr
+	BCHParams.PubKeyHashAddrID = 0x00 // 
