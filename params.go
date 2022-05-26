09541af41112b530d60828fe09ca586034426a0d
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-05-26 11:08:29 +0000

    remove unnecessary whitespace

--- params.go
+++ params.go
@@ -43,4 +43,4 @@ func init() {
 	BCHParams.PrivateKeyID = 0x80     // 128
 
 	// qtum net params
-	// https://github.com/qtumproject/qtum/blob/ma
+	// https://github.com/qtumproject/qtum/blob/master/src/ch
