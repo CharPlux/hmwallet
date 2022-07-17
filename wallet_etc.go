5eb8f5f027fa684a21394367d270ed6dd8147061
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-07-17 07:51:56 +0000

    Prepare for first release

--- wallet_etc.go
+++ wallet_etc.go
@@ -1,2 +1,19 @@
 
 package hdwallet
+
+import (
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func init() {
+	coins[ETC] = newETC
+}
+
+type etc struct {
+	name   string
+	symbol string
+	key    *Key
+}
+
+func newETC(key *Key) Wallet {
+	return &etc{
