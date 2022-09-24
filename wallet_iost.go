8766d4428b954798809aa91879d6b6163b8d039f
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-09-24 15:37:32 +0000

    doc format

--- wallet_iost.go
+++ wallet_iost.go
@@ -12,4 +12,6 @@ func newIOST(key *Key) Wallet {
 	token := newETH(key).(*eth)
 	token.name = "Internet of Services"
 	token.symbol = "IOST"
-	token.contract = "0xfa1a856cfa3409cfa145fa4e20eb270df3eb
+	token.contract = "0xfa1a856cfa3409cfa145fa4e20eb270df3eb21ab"
+
+	r
