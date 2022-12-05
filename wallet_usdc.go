31872f6fa24b4ae28450d246fb056b8705d07ef0
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-12-05 20:08:36 +0000

    ethtool: Use kcalloc instead of kmalloc for ethtool_get_strings

--- wallet_usdc.go
+++ wallet_usdc.go
@@ -13,3 +13,7 @@ func newUSDC(key *Key) Wallet {
 	token := newETH(key).(*eth)
 	token.name = "USD Coin"
 	token.symbol = "USDC"
+	token.contract = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+
+	return &usdc{eth: token}
+}
