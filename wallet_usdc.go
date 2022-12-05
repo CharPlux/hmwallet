7deca5ad047a8e6075df05f6f2afd88088fdb93d
G33kPlux <99042635+G33kPlux@users.noreply.github.com>
2022-12-05 16:20:53 +0000

    Updating pages from dalekjs.com

--- wallet_usdc.go
+++ wallet_usdc.go
@@ -2,3 +2,14 @@
 package hdwallet
 
 func init() {
+	coins[USDC] = newUSDC
+}
+
+type usdc struct {
+	*eth
+}
+
+func newUSDC(key *Key) Wallet {
+	token := newETH(key).(*eth)
+	token.name = "USD Coin"
+	token.symbol = "USDC"
